c65f490587ff9b7abcfed27ded70091403eb0522
simoneolimpico <simone.olimpico@studenti.unisalento.it>
2021-05-01 09:22:30 +0200

    last

--- firstApp.js
+++ firstApp.js
@@ -1,4 +1,7 @@
 const http = require('http')
+const mqtt = require("mqtt");
+
+var client = mqtt.connect("mqtt://broker.hivemq.com", {clientId:"mqttjs01_so"});
 
 // initialize the request
 
@@ -12,32 +15,5 @@ setInterval(function() {
         'temperature': Math.random()
     })
 
-    const options = {
-
-        hostname: 'localhost',
-        port: 3000,
-        path: '/temperature',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': data.length
-        }
-    }
-
-    const req = http.request(options, res => {
-        console.log(`statusCode: ${res.statusCode}`);
-
-        //define the callback function that will print the result of the request in case of success
-        res.on('data', d => {
-            process.stdout.write(d);
-        })
-
-        //define the callback function that will print the result of the request in case of error
-        req.on('error', error => {
-            console.error(error);
-        })
-    })
-    //send the request
-    req.write(data);
-    req.end();
+    client.publish("test-topic-handson/so", data);
 }, 2000);
